86e2552785ea54dd9663bdc09affd2d8f3eae835
Margaev <andreymargaev@gmail.com>
2021-01-25 17:02:26 +0300

    aws_hw deleted excess ingress rules in rds security group

--- aws_hw/tf_deploy/data_services.tf
+++ aws_hw/tf_deploy/data_services.tf
@@ -45,22 +45,6 @@ resource "aws_security_group" "postgres_rds_security_group" {
   description = "Allow TLS inbound traffic"
 
   ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     description = "postgres"
     from_port   = 5432
     to_port     = 5432
