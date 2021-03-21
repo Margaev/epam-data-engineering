0ed88f91a89ce3e36ba8f7a03e52c28a7fb0d794
Margaev <andreymargaev@gmail.com>
2021-03-22 02:22:13 +0300

    aws_hw remove comments

--- aws_hw/deploy_lambda.sh
+++ aws_hw/deploy_lambda.sh
@@ -1,6 +1,3 @@
-#mkdir -p lambda_layer/python/
-#sudo pip3 install -r requirements.txt -t lambda_layer/python/
-
 sudo rm -rf lambda_layer/python/ && mkdir -p lambda_layer/python/
 sudo docker run --rm -v $(pwd):/lambda_libs -w /lambda_libs lambci/lambda:build-python3.8 \
     pip install -r requirements.txt --no-deps -t lambda_layer/python/
